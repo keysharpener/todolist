11b56833c6ad48a35c0cc48a07d8921f704e8836
Robin Francillon <robin.francillon@gmail.com>
2016-04-05 23:25:58 +0200

    css working

--- webpack.config.js
+++ webpack.config.js
@@ -8,14 +8,23 @@ const PATHS = {
 const TARGET = process.env.npm_lifecycle_event;
 const webpack = require('webpack');
 const common = {
-  // Entry accepts a path or an object of entries. We'll be using the
-  // latter form given it's convenient with more complex configurations.
   entry: {
     app: PATHS.app
   },
   output: {
     path: PATHS.build,
     filename: 'bundle.js'
+    },
+  module: {
+    loaders: [
+      {
+        // Test expects a RegExp! Note the slashes!
+        test: /\.css$/,
+        loaders: ['style', 'css'],
+        // Include accepts either a path or an array of paths.
+        include: PATHS.app
+      }
+    ]
   }
 };
 
